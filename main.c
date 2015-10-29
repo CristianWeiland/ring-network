af6c776d4284db53ce7b43f4a7fa63f3e6c7ea26
Cristian Weiland <cw14@inf.ufpr.br>
2015-10-29 11:44:45 -0200

    Improve basic code
    Signed-off-by: Cristian Weiland <cw14@inf.ufpr.br>

--- main.c
+++ main.c
@@ -8,12 +8,16 @@
 #include <unistd.h>
 #include <poll.h>
 
-#define THIS 0 // It sinalizes which machine Im on.
 #define TOKEN_TIMEOUT 100 // How much time will I stay with the token?
 #define RECOVERY_TIMEOUT 1000 // How much time will I wait before I create a new token? I have to calculate it!
+#define PORT 47237 // The base port, used to get all 4 ports (which will be 47237,47238,47239,47240).
+#define BUF_MAX 1024
 
-int Token = -1,Machine=-1;
-int Next,Prev;
+int Token = -1; // -1 If I dont know if I have the token, 0 if I dont have, 1 if I have.
+int Machine = -1; // The number of my machine(1-4).
+int Next,Prev; // Number of the previous / next machine (1-4).
+int In,Out; // Number of the In port and the Out port (from my machine).
+int SockIn,SockOut; // Socket in = server socket, Socket out = client socket.
 
 int send_msg(char *s) {
 /* This function should send the string s to my neighbor and return 1 on success and 0 on failure. */
@@ -23,15 +27,18 @@ int remove_msg(char *s) {
 /* This function should remove the message I previously sent from the ring. Return 1 on success, 0 on failure. */
 }
 
-int add_buffer(char **buf,int *len,char *s) {
+int add_buffer(char **buf, int *len, int first, char *s) {
 /* Parameters: Buffer, how many strings I have on the buffer and the string I will add in the buffer.
    I should add the string s to my buffer, so I can send it when I get the token.
    Returns 1 on success, 0 on failure (unable to allocate memory). */
-    if(buf[*len] = malloc(sizeof(char) * (strlen(s) + 1)) == NULL) {
+    if(*len == BUF_MAX) // Buffer is full. I could, insted of return 0, realloc my buffer and make it a smart buffer. Maybe later.
+        return 0;
+    int pos = (*len + first) % 1024; // Circular line. (Line eh a traducao certa de fila?)
+    if(buf[pos] = malloc(sizeof(char) * (strlen(s) + 1)) == NULL) {
         puts("(add_buffer) Unable to allocate memory.");
         return 0;
     }
-    strcpy(buf[*len],s);
+    strcpy(buf[pos],s);
     (*len)++;
     return 1;
 }
@@ -57,8 +64,12 @@ int timedout() {
 /* Check if a timeout occured. */
 }
 
-int buffer_is_empty() {
-/* It will check if buf len is 0. Maybe we dont need this function. */
+int rem_buffer(char **buf, int *len, int *first) {
+/* This function will remove the first element from the buffer and update its length and first position. */
+    free(buf[first]); // Erasing data.
+    (*len)++;
+    ((*first)++) % BUF_MAX; // If first is 1024, it will become 0 again.
+    return 1;
 }
 
 int send_token() {
@@ -70,10 +81,12 @@ int main(int argc, char* arv[]) {
     fds[0] = { STDIN_FILENO, POLLIN|POLLPRI };
     fds[1].fd = Socket;
     int timeout_msecs = 500;
-    int i=0;
+    int i=0,bufLen = 0,bufFirst = 0;
+    char **buf;
     char *s;
     s = malloc(1024);
     s[0] = '\0';
+
 /*
     int sockdescr;
     int numbytesrecv;
@@ -83,53 +96,75 @@ int main(int argc, char* arv[]) {
     char *host;
     char *dados;
 */
+
     if(argc != 1) {
         puts("Correct way to opearate: <Machine number>");
         return -1;
     }
 
-    Machine = args[1] - 33; // This probably does not work. Check it later please.
+/*
+    Machine     Port(in) Port(out)
+    1           PORT     PORT+1
+    2           PORT+1   PORT+2
+    3           PORT+2   PORT+3
+    4           PORT+3   PORT
+*/
+
+    Machine = args[1][0] - 48;
     if(Machine == 1) {
         create_token();
         Token = 1;
         Next = port+1;
         Prev = port+3;
+        In = PORT + Machine - 1;
+        Out = PORT + Machine;
     } else if(Token == 4) {
         Next = port-3;
         Prev = port-1;
         Token = 0;
-    } else {
+        In = PORT + Machine - 1;
+        Out = PORT;
+    } else if(Token == 2 || Token == 3) {
         Next = port+1;
         Prev = port-1;
-        Token =0;
-    }
-
-    create_server(port);
-
-    puts("When all machines have set up the server, type 1 to start connecting clients.");
-    scanf("%d",&i);
-    if(i != 1) {
-        puts("Some problem occured when setting up clients. Exiting...");
+        Token = 0;
+        In = PORT + Machine - 1;
+        Out = PORT + Machine;
+    } else {
+        puts("Please choose a number between 1 and 4.");
         return -1;
     }
 
-    create_client(port);
+    create_server(In); // I do not need to use this parameter, its just to remember that I will have to use it in this function.
+
+    puts("When all machines have set up the server, type any number to start connecting clients.");
+    scanf("%d",&i);
+
+    create_client(Out);
 
     while(1) {
+        while(Token && bufLen > 0) {
+            while(!send_msg(buf[0]));
+            while(!remove_msg(buf[0]);
+            remove_from_buffer(buf,&bufLen,&bufFirst);
+            if(timedout() == 1) { // Token time out.
+                send_token();
+                set_timeout(RECOVERY_TIMEOUT);
+            }
+        }
         if( poll(fds, 2, timeout_msecs) ) { // There is something to be read. Message or stdin.
             if(fds[0].revents & POLLIN|POLLPRI) { // Got something in STDIN.
                 fgets(s,1023,stdin); // Is 1023 enough?
-                if(Token == THIS) {
+                if(Token) {
                     while(!send_msg(s)); // Send a message with my string s.
                     while(!remove_msg(s));
                 } else {
-                    add_buffer(s);
+                    add_buffer(buf,&bufLen,first,s);
                 }
-                                    // I have to search this & with datagram socket possibly. Is it pollin?
-            } else if(fds[1].revents & POLLIN) { // Got a message!
+            } else if(fds[1].revents & POLLIN) { // Got a message! (Remember to check this & (AND).
                 receive_msg(); // What structure is my message? Could it be only a string?
                 if(typeof_msg() == 't') { // t is for token.
-                    Token = THIS;
+                    Token = 1;
                     set_timeout(TOKEN_TIMEOUT);
                 } else {
                     print_message();
@@ -138,17 +173,10 @@ int main(int argc, char* arv[]) {
             } else {
                 // Got nothing. Do nothing.
             }
-            if(timedout() || buffer_is_empty()) {
+            if(timedout() || bufLen == 0) {
                 send_token();
                 set_timeout(RECOVERY_TIMEOUT);
             }
         }
-            /*
-            scanf("%s",s);
-            printf("Li: %s\n",s);
-        } else { // There is nothing in STDIN.
-            puts("Nada nos parametros.");
-        }
-        i++;*/
     }
 }
